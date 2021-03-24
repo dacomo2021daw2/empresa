86c41b37dd4d8f77189c2a64b88460e68d7c212e
dacomo2021daw2 <dacomo2021daw2@protonmail.com>
2021-03-24 16:20:55 +0100

    Primer commit del projecte empresa

---
+++ empleats/resources/views/index.blade.php
@@ -0,0 +1,41 @@
+@extends('disseny')
+
+@section('content')
+
+<div class="mt-5">
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div>
+  @endif
+  <table class="table">
+    <thead>
+        <tr class="table-primary">
+          <td># ID</td>
+          <td>Nom</td>
+          <td>Email</td>
+          <td>Telèfon</td>
+          <td>Action</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($empleat as $empl)
+        <tr>
+            <td>{{$empl->id}}</td>
+            <td>{{$empl->name}}</td>
+            <td>{{$empl->email}}</td>
+            <td>{{$empl->phone}}</td>
+            <td class="text-center">
+                <a href="{{ route('empleats.edit', $empl->id)}}" class="btn btn-success btn-sm">Edita</a>
+                <form action="{{ route('empleats.destroy', $empl->id)}}" method="post" style="display: inline-block">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm" type="submit">Esborra</button>
+                  </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+@endsection
---
+++ empleats/resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+@extends('disseny')
+
+@section('content')
+
+<div class="card mt-5">
+  <div class="card-header">
+    Create User
+  </div>
+
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div>
+    @endif
+      <form method="post" action="{{ route('empleats.store') }}">
+          <div class="form-group">
+              @csrf
+              <label for="nom">Nom</label>
+              <input type="text" class="form-control" name="nom"/>
+          </div>
+          <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" name="email"/>
+          </div>
+          <div class="form-group">
+              <label for="telefon">Telèfon</label>
+              <input type="tel" class="form-control" name="telefon"/>
+          </div>
+          <button type="submit" class="btn btn-block btn-primary">Envia</button>
+      </form>
+  </div>
+</div>
+@endsection
